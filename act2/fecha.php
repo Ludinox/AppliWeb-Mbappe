0a8336691a40f35585a1828ca2df9ddfb003226a
Ludinox <114451029+Ludinox@users.noreply.github.com>
2022-10-19 17:27:09 -0500

    Update fecha.php

--- act2/fecha.php
+++ act2/fecha.php
@@ -6,10 +6,11 @@
   <title>Fecha</title>
 </head>
 <body>
+  <!--Hoja de estilo y div para marcar la clase de la cuota en este caso-->
  <link rel="stylesheet" type="text/css" href="style.css">
 <div class="blockquote-wrapper">
   <div class="blockquote">
-    
+    <!--Insertar la hora como tal ademas de datos de mi, Adrian Perez-->
     <h1>
      La Fecha es: <span style="color:#ffffff"><?php 
 echo date('d-m-y');
